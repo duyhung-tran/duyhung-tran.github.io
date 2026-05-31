4f1634e48fa14008e14625a7a3bd153201be4215
Hung Tran <tranndhungg@gmail.com>
2026-05-31 15:23:57 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-// };
-
-// export default nextConfig;
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/portfolio',
+  basePath: '/works',
 };
 
 module.exports = nextConfig;
