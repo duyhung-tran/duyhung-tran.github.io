7d9f5a1722a39b08e9963bfecc0070c34ae370e8
Hung Tran <tranndhungg@gmail.com>
2026-06-06 13:12:04 -0700

    uhhh idk

---
+++ .next/dev/static/chunks/app_page_tsx_0g0skae._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/_0h17do.._.js","static/chunks/node_modules_0zw-z1z._.js"],
+    source: "dynamic"
+});
